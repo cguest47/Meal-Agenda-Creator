aa6b32e16752a6e8df3a203d349de206bfc8fcf7
cguest47 <99707954+cguest47@users.noreply.github.com>
2023-03-14 22:38:23 -0500

    Dinner Recipes file and Show Results in a Server

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
-const dinnerArray = [
-    {name: 'Butter Chicken'},
-    {name: 'Ramen'},
-    {name: 'Frozen Pizza'},
-    {name: 'Chicken and Rice'},
-    {name: 'Steaks'},
-    {name: 'Chili'},
-    {name: 'Spaghetti'},
-    {name: 'Tacos'},
-    {name: 'Kelsee\'s Tortellini'},
-    {name: 'Stir Fry'}
-];
+const http = require('node:http');
+const {dinnerArray} = require('./dinnerRecipes');
+
+const hostname = '127.0.0.1';
+const port = 3000;
 
 const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
@@ -22,15 +15,27 @@ const createWeeklyMeal = (day, mealName) => {
 
 const weeklyDinnerAgenda = (daysOfWeek, dinnerArray) => {
     let agenda = [];
-    for (let index = 0; index < 7; index++) {
+    for (let index = 0; index < daysOfWeek.length; index++) {
         const currentDay = daysOfWeek[index];
         const currentRandomIndex = Math.floor(Math.random() * dinnerArray.length);
         const currentMeal = dinnerArray.splice(currentRandomIndex, 1);
         const newMeal = createWeeklyMeal(currentDay, currentMeal[0].name);
-        agenda += newMeal;
+        agenda.push(newMeal);
         console.log(newMeal);
     }
-    return agenda;
+    console.log(agenda);
+    return JSON.stringify(agenda);
 };
 
 const weeklyAgenda = weeklyDinnerAgenda(daysOfWeek,dinnerArray);
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(weeklyAgenda);
+  });
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+
---
+++ dinnerRecipes.js
@@ -0,0 +1,14 @@
+const dinnerArray = [
+    {name: 'Butter Chicken'},
+    {name: 'Ramen'},
+    {name: 'Frozen Pizza'},
+    {name: 'Chicken and Rice'},
+    {name: 'Steaks'},
+    {name: 'Chili'},
+    {name: 'Spaghetti'},
+    {name: 'Tacos'},
+    {name: 'Kelsee\'s Tortellini'},
+    {name: 'Stir Fry'}
+];
+
+module.exports = {dinnerArray};
